3139552391bf789605c9ce6ae12f818a479518c6
Kloong1 <dkdlsmsm98@gmail.com>
2022-07-05 23:24:56 +0900

    Spring MVC Web - Message & Internationalization

--- Spring_MVC_web/projects/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ Spring_MVC_web/projects/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -50,6 +50,7 @@ public class MessageSourceTest {
     @Test
     void enLang() {
         assertThat(messageSource.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+        assertThat(messageSource.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
         assertThat(messageSource.getMessage("hello.name", new Object[]{"Spring"}, Locale.ENGLISH)).isEqualTo("hello Spring");
     }
 }
