810f21c0472a8695627c389e7cf472b3baa6793b
Kloong1 <dkdlsmsm98@gmail.com>
2022-07-28 21:43:04 +0900

    Spring MVC Web - Bean Validation

--- Spring_MVC_web/projects/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring_MVC_web/projects/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,30 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000",
+//                message = "총 합 10000원 넘게 입력 ㄱㄱ")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
---
+++ Spring_MVC_web/projects/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -0,0 +1,142 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v3/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV3 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v3/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v3/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v3/addForm";
+    }
+
+    //@PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        //여러 필드에 대한 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int value = item.getPrice() * item.getQuantity();
+            if (value < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, value}, null);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/addForm";
+        }
+        //검증 성공시 로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        //여러 필드에 대한 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int value = item.getPrice() * item.getQuantity();
+            if (value < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, value}, null);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/addForm";
+        }
+        //검증 성공시 로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v3/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        //여러 필드에 대한 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int value = item.getPrice() * item.getQuantity();
+            if (value < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, value}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    //@PostMapping("/{itemId}/edit")
+    public String editV2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+
+        //여러 필드에 대한 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int value = item.getPrice() * item.getQuantity();
+            if (value < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, value}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+}
+
