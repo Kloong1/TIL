b66241264f70d4d2ba149279c3f0531c278b2b77
Kloong1 <dkdlsmsm98@gmail.com>
2022-09-23 17:14:52 +0900

    Spring DB 1 - Exception

---
+++ Spring_db_1/projects/jdbc/src/test/java/hello/jdbc/exception/basic/CheckedExceptionAppTest.java
@@ -0,0 +1,51 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class CheckedExceptionAppTest {
+
+    @Test
+    void checkedTest() {
+        Controller controller = new Controller();
+        assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() throws SQLException, ConnectException {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class NetworkClient {
+        public void call() throws ConnectException {
+            throw new ConnectException("ex");
+        }
+    }
+
+}
