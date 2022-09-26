015aaca8350c693bdfc945a1b14fbf550da160ca
Kloong1 <dkdlsmsm98@gmail.com>
2022-09-26 20:46:39 +0900

    fix typo

---
+++ PS/2022_kakao/project/src/typing/Typing.java
@@ -0,0 +1,18 @@
+package typing;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Typing {
+    public static void main(String[] args) {
+        String[] strings = new String[3];
+
+
+    }
+
+    private void method() throws SQLException {
+        Typing[] test = new Typing[3];
+
+        System.out.println("Hello world!");
+    }
+}
